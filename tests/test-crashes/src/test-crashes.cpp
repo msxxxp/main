13cdb38875a65861ff8f992305c9391a3e573089
andrey.grechkin <andrey.grechkin@192.168.23.53>
2014-10-10 17:45:43 +0400

    Signed-off-by: andrey.grechkin <andrey.grechkin@192.168.23.53>

---
+++ tests/test-crashes/src/test-crashes.cpp
@@ -0,0 +1,56 @@
+#include <basis/sys/console.hpp>
+#include <basis/sys/logger.hpp>
+#include <basis/sys/totext.hpp>
+#include <basis/sys/traceback.hpp>
+
+#include <excis/exception.hpp>
+
+#include <basis/simstd/string>
+
+namespace {
+	void setup_logger()
+	{
+		LogSetOptions(L"logger:///default?level=tr;prefix=fu;target=co");
+
+		traceback::init();
+	}
+
+	void do_av_exception()
+	{
+		LogWarn(L"accessing null pointer\n");
+		volatile int* p = nullptr;
+		*p = 0;
+	}
+}
+
+extern "C" int wmain(int argc, wchar_t * argv[])
+{
+	UNUSED(argc);
+	UNUSED(argv);
+
+	console::printf(L"%S:%d\n", __PRETTY_FUNCTION__, __LINE__);
+
+	setup_logger();
+
+	exception::set_vectored_exception_filter();
+	exception::set_unhandled_exception_filter();
+
+	try {
+
+		LogTrace();
+
+		do_av_exception();
+
+	} catch (exception::AbstractError & e) {
+		console::printf(L"SEH exception cought: %s\n", e.what().c_str());
+	} catch (...) {
+		console::printf(L"cpp exception cought\n");
+	}
+
+	return 0;
+}
+
+int WINAPI wWinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPWSTR /*lpCmdLine*/, int /*nShowCmd*/)
+{
+	return wmain(0, nullptr);
+}

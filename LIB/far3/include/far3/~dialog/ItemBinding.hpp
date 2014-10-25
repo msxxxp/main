107deaef6d6d8ab182bc109059387aa3755f3080
andrew.grechkin <andrew.grechkin@gmail.com>
2014-10-25 08:52:42 +0400

    Signed-off-by: andrew.grechkin <andrew.grechkin@gmail.com>

---
+++ LIB/far3/include/far3/~dialog/ItemBinding.hpp
@@ -0,0 +1,73 @@
+﻿/**
+ © 2014 Andrew Grechkin
+ Source code: <http://code.google.com/p/andrew-grechkin>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#ifndef _FAR3_DIALOG_ITEMBINDING_HPP_
+#define _FAR3_DIALOG_ITEMBINDING_HPP_
+
+#include <far3/~3rdparty/plugin.hpp>
+//#include <far3/helper.hpp>
+
+#include <basis/ext/pattern.hpp>
+
+namespace far3 {
+	namespace dialog {
+
+		class ItemBinding {
+		public:
+			virtual ~ItemBinding() = default;
+
+			HANDLE get_dlg() const;
+
+			ssize_t get_index() const;
+
+			void set_dlg(HANDLE * hndl);
+
+			void set_index(ssize_t index);
+
+			void save() const;
+
+			ssize_t get_height() const;
+
+			ssize_t get_width() const;
+
+		protected:
+			ItemBinding() :
+				m_dlg(), m_index()
+			{
+			}
+
+			ItemBinding(HANDLE * dlg, ssize_t index) :
+				m_dlg(dlg), m_index(index)
+			{
+			}
+
+		private:
+			virtual void save_() const = 0;
+
+			virtual ssize_t get_height_() const;
+
+			virtual ssize_t get_width_() const = 0;
+
+			HANDLE * m_dlg;
+			ssize_t m_index;
+		};
+
+	}
+}
+
+#endif
---
+++ LIB/far3/src/~dialog/ItemBinding.cpp
@@ -0,0 +1,68 @@
+﻿/**
+ © 2014 Andrew Grechkin
+ Source code: <http://code.google.com/p/andrew-grechkin>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#include <far3/dialog.hpp>
+
+namespace far3 {
+	namespace dialog {
+
+		HANDLE ItemBinding::get_dlg() const
+		{
+			return (this) ? *m_dlg : nullptr;
+		}
+
+		ssize_t ItemBinding::get_index() const
+		{
+			return (this) ? m_index : 0;
+		}
+
+		void ItemBinding::set_dlg(HANDLE * hndl)
+		{
+			if (this)
+				m_dlg = hndl;
+		}
+
+		void ItemBinding::set_index(ssize_t index)
+		{
+			if (this)
+				m_index = index;
+		}
+
+		void ItemBinding::save() const
+		{
+			if (this)
+				save_();
+		}
+
+		ssize_t ItemBinding::get_height() const
+		{
+			return (this) ? get_height_() : 1;
+		}
+
+		ssize_t ItemBinding::get_width() const
+		{
+			return (this) ? get_width_() : 0;
+		}
+
+		ssize_t ItemBinding::get_height_() const
+		{
+			return 1;
+		}
+
+	}
+}

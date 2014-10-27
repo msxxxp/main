8533fbc28db877c313bda3cc96cebb6849b5533b
andrew.grechkin <andrew.grechkin@gmail.com>
2014-10-27 19:26:43 +0400

    Signed-off-by: andrew.grechkin <andrew.grechkin@gmail.com>

---
+++ LIB/far3/include/far3/viewer.hpp
@@ -0,0 +1,26 @@
+﻿
+/**
+ © 2014 Andrew Grechkin
+ Source code: <http://code.google.com/p/andrew-grechkin>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#ifndef _FAR3_VIEWER_HPP_
+#define _FAR3_VIEWER_HPP_
+
+#include <far3/plugin.hpp>
+#include <far3/~viewer/common.hpp>
+
+#endif

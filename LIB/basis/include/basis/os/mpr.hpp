6f13c42d4aa8b9d7e43d8119e9ddc5701f50ac74
andrew.grechkin <andrew.grechkin@gmail.com>
2014-11-23 19:36:40 +0400

    Signed-off-by: andrew.grechkin <andrew.grechkin@gmail.com>

---
+++ LIB/basis/include/basis/os/mpr.hpp
@@ -0,0 +1,23 @@
+﻿#ifndef BASIS_OS_MPR_
+#define BASIS_OS_MPR_
+
+#include <basis/sys/linkage.hpp>
+
+namespace os {
+
+	struct mpr_dll: private linkage::DynamicLibrary {
+		typedef DWORD (WINAPI *FWNetAddConnection2W)(LPNETRESOURCEW, LPCWSTR, LPCWSTR, DWORD);
+		typedef DWORD (WINAPI *FWNetCancelConnection2W)(LPCWSTR, DWORD, WINBOOL);
+
+		DEFINE_FUNC(WNetAddConnection2W);
+		DEFINE_FUNC(WNetCancelConnection2W);
+
+		static mpr_dll& inst();
+
+	private:
+		mpr_dll();
+	};
+
+}
+
+#endif
---
+++ LIB/basis/src/os/mpr.cpp
@@ -0,0 +1,18 @@
+﻿#include <basis/os/mpr.hpp>
+
+namespace os {
+
+	mpr_dll& mpr_dll::inst()
+	{
+		static mpr_dll ret;
+		return ret;
+	}
+
+	mpr_dll::mpr_dll() :
+		DynamicLibrary(L"mpr.dll")
+	{
+		GET_DLL_FUNC(WNetAddConnection2W);
+		GET_DLL_FUNC(WNetCancelConnection2W);
+	}
+
+}

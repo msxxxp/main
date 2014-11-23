87a53b60f09b8b18a792adfbfcc95c8cdba3beb1
andrew.grechkin <andrew.grechkin@gmail.com>
2014-11-23 15:36:00 +0400

    Signed-off-by: andrew.grechkin <andrew.grechkin@gmail.com>

--- tests/test-basis/src/test-basis.cpp
+++ tests/test-basis/src/test-basis.cpp
@@ -11,15 +11,6 @@ namespace {
 	}
 }
 
-int WINAPI wWinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPWSTR /*lpCmdLine*/, int /*nShowCmd*/)
-{
-	console::printf(L"%S:%d\n", __PRETTY_FUNCTION__, __LINE__);
-
-	setup_logger();
-
-	return 0;
-}
-
 extern "C" int wmain(int argc, wchar_t* argv[])
 {
 	console::printf(L"%S:%d\n", __PRETTY_FUNCTION__, __LINE__);
@@ -30,11 +21,11 @@ extern "C" int wmain(int argc, wchar_t* argv[])
 	for (int i = 0; i < argc; ++i)
 		LogDebug(L"'%s'\n", argv[i]);
 
-//	test_logger();
-//
-//	test_chrono();
+	test_logger();
 
-	test_console_input();
+//	test_chrono();
+//
+//	test_console_input();
 //
 //	test_traceback();
 //
@@ -45,4 +36,9 @@ extern "C" int wmain(int argc, wchar_t* argv[])
 //	test_window();
 
 	return 0;
+}
+
+int WINAPI wWinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPWSTR /*lpCmdLine*/, int /*nShowCmd*/)
+{
+	return wmain(0, nullptr);
 }

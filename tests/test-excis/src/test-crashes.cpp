2e5ad84cb53ca9fe035d887c831c2d4ff81f9663
andrew.grechkin <andrew.grechkin@gmail.com>
2014-11-30 12:16:14 +0400

    Signed-off-by: andrew.grechkin <andrew.grechkin@gmail.com>

--- tests/test-excis/src/test-crashes.cpp
+++ tests/test-excis/src/test-crashes.cpp
@@ -4,6 +4,14 @@
 #include <basis/sys/logger.hpp>
 #include <basis/simstd/string>
 
+#include <excis/exception.hpp>
+
+void do_cpp_exception()
+{
+	LogWarn(L"throwning 42\n");
+	throw 42;
+}
+
 void do_null_string()
 {
 	LogWarn(L"length null sting\n");
@@ -34,23 +42,71 @@ void test_null_string()
 	LogTrace();
 }
 
+void test_cpp_exception()
+{
+	LogTrace();
+
+	try {
+		do_cpp_exception();
+	} catch (exception::AbstractError & e) {
+		LogFatal(L"SEH exception cought: %s\n", e.what().c_str());
+		LogFatal(L"SEH exception cought: %s\n", e.where().c_str());
+
+		auto mstr = e.format_error();
+		for (size_t i = 0; i < mstr.size(); ++i)
+			LogFatal(L"\t%s\n", mstr[i]);
+	} catch (...) {
+		LogFatal(L"cpp exception cought\n");
+	}
+
+	LogTrace();
+}
+
 void test_access_violation()
 {
 	LogTrace();
-	do_acces_violation();
+
+	try {
+		do_acces_violation();
+	} catch (exception::AbstractError & e) {
+		LogFatal(L"SEH exception cought: %s\n", e.what().c_str());
+		LogFatal(L"SEH exception cought: %s\n", e.where().c_str());
+
+		auto mstr = e.format_error();
+		for (size_t i = 0; i < mstr.size(); ++i)
+			LogFatal(L"\t%s\n", mstr[i]);
+	} catch (...) {
+		LogFatal(L"cpp exception cought\n");
+	}
+
 	LogTrace();
 }
 
 void test_zero_division()
 {
 	LogTrace();
-	do_zero_division();
+
+	try {
+		do_zero_division();
+	} catch (exception::AbstractError & e) {
+		LogFatal(L"SEH exception cought: %s\n", e.what().c_str());
+		LogFatal(L"SEH exception cought: %s\n", e.where().c_str());
+
+		auto mstr = e.format_error();
+		for (size_t i = 0; i < mstr.size(); ++i)
+			LogFatal(L"\t%s\n", mstr[i]);
+	} catch (...) {
+		LogFatal(L"cpp exception cought\n");
+	}
+
 	LogTrace();
 }
 
+/// WARNING for x64 Optimization must be -O0 or it will crash (tdm compiler)
+
 void test_crashes()
 {
-	test_null_string();
+//	test_null_string();
 
 	test_zero_division();
 

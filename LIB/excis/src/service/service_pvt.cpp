2e0bcb46ec7b7309f0cf74c4817e81b02a53a1f7
andrey.grechkin <andrew.grechkin@gmail.com>
2014-12-02 19:19:01 +0400

    Signed-off-by: andrey.grechkin <andrew.grechkin@gmail.com>

---
+++ LIB/excis/src/service/service_pvt.cpp
@@ -0,0 +1,27 @@
+﻿#include "service_pvt.hpp"
+#include <excis/exception.hpp>
+
+#include <basis/sys/logger.hpp>
+
+namespace service {
+
+	memory::auto_buf<LPQUERY_SERVICE_CONFIGW> QueryConfig(SC_HANDLE hndl)
+	{
+		LogNoise2(L"handle: %p\n", hndl);
+		DWORD bytesNeeded = 0;
+		CheckApi(!::QueryServiceConfigW(hndl, nullptr, 0, &bytesNeeded) && ::GetLastError() == ERROR_INSUFFICIENT_BUFFER);
+		memory::auto_buf<LPQUERY_SERVICE_CONFIGW> buf(bytesNeeded);
+		CheckApi(::QueryServiceConfigW(hndl, buf, buf.size(), &bytesNeeded));
+		return buf;
+	}
+
+	memory::auto_buf<PBYTE> QueryConfig2(SC_HANDLE hndl, DWORD level)
+	{
+		LogNoise2(L"handle: %p\n", hndl);
+		DWORD bytesNeeded = 0;
+		CheckApi(!::QueryServiceConfig2W(hndl, level, nullptr, 0, &bytesNeeded) && ::GetLastError() == ERROR_INSUFFICIENT_BUFFER);
+		memory::auto_buf<PBYTE> buf(bytesNeeded);
+		CheckApi(::QueryServiceConfig2W(hndl, level, buf, buf.size(), &bytesNeeded));
+		return buf;
+	}
+}
---
+++ LIB/excis/src/service/service_pvt.hpp
@@ -0,0 +1,11 @@
+﻿#include <excis/service.hpp>
+
+#include <basis/sys/memory.hpp>
+
+namespace service {
+
+	memory::auto_buf<LPQUERY_SERVICE_CONFIGW> QueryConfig(SC_HANDLE hndl);
+
+	memory::auto_buf<PBYTE> QueryConfig2(SC_HANDLE hndl, DWORD level);
+
+}

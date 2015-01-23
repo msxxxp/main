53533f965c7743c597a657fcfd893aecf98ae5c7
andrey.grechkin <andrew.grechkin@gmail.com>
2015-01-23 18:52:00 +0400

    Signed-off-by: andrey.grechkin <andrew.grechkin@gmail.com>

--- tests/test-basis/src/test-list.cpp
+++ tests/test-basis/src/test-list.cpp
@@ -11,73 +11,70 @@
 
 void test_list()
 {
-	LogTrace();
+	LogAtten(L"\n");
 
-	A ma[7];
-
-	LogTrace();
+//	A ma[7];
 
 	struct HeapTag {};
-	typedef memory::heap::DecoratorStat<memory::heap::Default, memory::heap::StatCount, HeapTag> heap_type;
+	typedef memory::heap::DecoratorStat<memory::heap::Special<HeapTag>, memory::heap::StatLog, HeapTag> heap_type;
 	typedef simstd::AllocatorHeap<A, heap_type> EqAlloc;
 
 	heap_type::init();
 	{
-
 		LogTrace();
+		typedef simstd::list<A, EqAlloc> test_list_type;
 		A a;
 
-		typedef simstd::list<A, EqAlloc> test_list_type;
 		test_list_type list1;
-		LogInfo(L"1:size(): %Iu\n", list1.size());
-		LogInfo(L"1:empty(): %d\n", list1.empty());
+		LogReport(L"1:size(): %Iu\n", list1.size());
+		LogReport(L"1:empty(): %d\n", list1.empty());
 
 		list1.emplace_back(1);
-		LogInfo(L"1:size(): %Iu\n", list1.size());
-		LogInfo(L"1:empty(): %d\n", list1.empty());
+		LogReport(L"1:size(): %Iu\n", list1.size());
+		LogReport(L"1:empty(): %d\n", list1.empty());
 
 		list1.emplace_back(2);
-		LogInfo(L"1:size(): %Iu\n", list1.size());
-		LogInfo(L"1:empty(): %d\n", list1.empty());
+		LogReport(L"1:size(): %Iu\n", list1.size());
+		LogReport(L"1:empty(): %d\n", list1.empty());
 
 		list1.emplace_back(3);
-		LogInfo(L"1:size(): %Iu\n", list1.size());
-		LogInfo(L"1:empty(): %d\n", list1.empty());
+		LogReport(L"1:size(): %Iu\n", list1.size());
+		LogReport(L"1:empty(): %d\n", list1.empty());
 
 		list1.reverse();
 
 		list1.pop_back();
-		LogInfo(L"1:size(): %Iu\n", list1.size());
-		LogInfo(L"1:empty(): %d\n", list1.empty());
+		LogReport(L"1:size(): %Iu\n", list1.size());
+		LogReport(L"1:empty(): %d\n", list1.empty());
 
 		for (auto it = list1.begin(); it != list1.end(); ++it)
-			LogInfo(L"%d\n", it->val());
+			LogReport(L"%d\n", it->val());
 
 		test_list_type list2(5, a);
-		LogInfo(L"2:size(): %Iu\n", list2.size());
-		LogInfo(L"2:empty(): %d\n", list2.empty());
+		LogReport(L"2:size(): %Iu\n", list2.size());
+		LogReport(L"2:empty(): %d\n", list2.empty());
 
 		test_list_type list3(10);
-		LogInfo(L"3:size(): %Iu\n", list3.size());
-		LogInfo(L"3:empty(): %d\n", list3.empty());
+		LogReport(L"3:size(): %Iu\n", list3.size());
+		LogReport(L"3:empty(): %d\n", list3.empty());
 
 		test_list_type list4(simstd::begin(ma), simstd::end(ma));
-		LogInfo(L"4:size(): %Iu\n", list4.size());
-		LogInfo(L"4:empty(): %d\n", list4.empty());
+		LogReport(L"4:size(): %Iu\n", list4.size());
+		LogReport(L"4:empty(): %d\n", list4.empty());
 
 		test_list_type list4a(list4.begin(), list4.end());
-		LogInfo(L"4a:size(): %Iu\n", list4a.size());
-		LogInfo(L"4a:empty(): %d\n", list4a.empty());
+		LogReport(L"4a:size(): %Iu\n", list4a.size());
+		LogReport(L"4a:empty(): %d\n", list4a.empty());
 
 		test_list_type list5(list2);
-		LogInfo(L"5:size(): %Iu\n", list5.size());
-		LogInfo(L"5:empty(): %d\n", list5.empty());
+		LogReport(L"5:size(): %Iu\n", list5.size());
+		LogReport(L"5:empty(): %d\n", list5.empty());
 
 		test_list_type list6(simstd::move(list3));
-		LogInfo(L"6:size(): %Iu\n", list6.size());
-		LogInfo(L"6:empty(): %d\n", list6.empty());
-		LogInfo(L"3:size(): %Iu\n", list3.size());
-		LogInfo(L"3:empty(): %d\n", list3.empty());
+		LogReport(L"6:size(): %Iu\n", list6.size());
+		LogReport(L"6:empty(): %d\n", list6.empty());
+		LogReport(L"3:size(): %Iu\n", list3.size());
+		LogReport(L"3:empty(): %d\n", list3.empty());
 
 		list1.emplace_back(a);
 
@@ -86,6 +83,10 @@ void test_list()
 		list1.push_back(a + a);
 
 		list1.push_back(simstd::move(a));
+
+//		test_list_type list6;
+//		LogReport(L"6:empty(): %d\n", list6.empty());
+//		list6.erase(list6.begin());
 	}
 	heap_type::destroy();
 
